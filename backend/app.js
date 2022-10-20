56642b7b029975a00fc0305a9209d5b47ba749fa
Palisha Shakya <palishashakya@gmail.com>
2022-10-20 13:46:12 +0545

    mysql db connected

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,46 @@
+require("dotenv").config();
 const express = require("express");
-const dotenv = require("dotenv");
+const mysql = require("mysql");
+const path = require("path");
+// const pool = require("./config/db");
 
 const app = express();
-dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+});
+app.set("view engine", "hbs");
+
+const publicDirectory = path.join(__dirname, "./frontend/src");
+app.use(express.static(publicDirectory));
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("MYSQL Connected");
+  }
+});
+//middleware
+app.use(express.json());
 
 const employees = require("./data/employees");
 
 app.get("/", (req, res) => {
   res.send("API is running..");
 });
-
+app.get("/dashboard", (req, res) => {
+  res.send("Dashboard Page");
+});
+app.get("/admin", (req, res) => {
+  res.send("Admin Page");
+});
 app.get("/api/employees", (req, res) => {
   res.json(employees);
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port :http://localhost:${PORT}`)
