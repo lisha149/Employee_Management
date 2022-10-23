48f3f75682dcfdbeb1f8cf8f54f212c161c2c725
Palisha Shakya <palishashakya@gmail.com>
2022-10-23 15:55:13 +0545

    login routes added in userRoutes

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const Models = require("./models");
-const bcrypt = require("bcrypt");
-const User = Models.users;
 
-const jwt = require("jsonwebtoken");
 const db = require("./models/index");
-const cors = require("cors");
 var usersRouter = require("./routes/userRoutes");
 var deptRouter = require("./routes/deptRoutes");
 
 const app = express();
-
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,48 +15,10 @@ app.get("/", (req, res) => {
 });
 
 //Login API
-app.post("/login", async (req, res, next) => {
-  if (req.body.email == "" || req.body.password == "") {
-    res
-      .status(400)
-      .json({ error: "Email address or password cannot be empty" });
-  }
+// app.use("/login", usersRouter);
 
-  const user = await User.findOne({
-    where: { email: req.body.email },
-  });
-
-  if (user) {
-    const password = req.body.password;
-    const password_valid = await bcrypt.compare(password, user.password);
-    if (password_valid) {
-      token = jwt.sign(
-        {
-          user_id: req.body.user_id,
-          first_name: req.body.first_name,
-          last_name: req.body.last_name,
-          email: req.body.email,
-          is_admin: req.body.is_admin,
-        },
-        process.env.JWT_SECRET
-      );
-      res.status(200).json({
-        id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email,
-        is_admin: user.is_admin,
-        token: token,
-      });
-    } else {
-      res.status(400).json({ error: "Password Incorrect" });
-    }
-  } else {
-    res.status(404).json({ error: "User does not exist" });
-  }
-});
 //Add users
-app.use("/employee", usersRouter);
+app.use("/api", usersRouter);
 app.use("/department", deptRouter);
 
 const PORT = process.env.PORT || 5000;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,52 @@ var router = express.Router();
 const Models = require("../models");
 const bcrypt = require("bcrypt");
 const User = Models.users;
+const jwt = require("jsonwebtoken");
 
-router.post("/", async (req, res, next) => {
+//Login API
+router.post("/login", async (req, res, next) => {
+  if (req.body.email == "" || req.body.password == "") {
+    res
+      .status(400)
+      .json({ error: "Email address or password cannot be empty" });
+  }
+
+  const user = await User.findOne({
+    where: { email: req.body.email },
+  });
+
+  if (user) {
+    const password = req.body.password;
+    const password_valid = await bcrypt.compare(password, user.password);
+    if (password_valid) {
+      let token = jwt.sign(
+        {
+          user_id: req.body.user_id,
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          is_admin: req.body.is_admin,
+        },
+        process.env.JWT_SECRET
+      );
+      res.status(200).json({
+        id: user.id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        is_admin: user.is_admin,
+        token: token,
+      });
+    } else {
+      res.status(400).json({ error: "Password Incorrect" });
+    }
+  } else {
+    res.status(404).json({ error: "User does not exist" });
+  }
+});
+//Add Employee
+
+router.post("/employee", async (req, res, next) => {
   const salt = await bcrypt.genSalt(10);
   var usr = {
     id: req.body.id,
