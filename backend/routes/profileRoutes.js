be62313f0493deee729a9b95daa7cdc51fda2602
Palisha Shakya <palishashakya@gmail.com>
2022-11-01 15:51:08 +0545

    added name in profile model

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -2,72 +2,61 @@ var express = require("express");
 var router = express.Router();
 const Models = require("./../models");
 const Profile = Models.profiles;
+const User = Models.users;
 const { isAuth, isAdmin } = require("../middleware/authMiddleware");
 //Create
-router.post("/profile", isAdmin, async (req, res, next) => {
-  const {
-    id,
-    user_id,
-    first_name,
-    last_name,
-    email,
-    address,
-    contact_number,
-    dob,
-    citizenship_number,
-    pan_number,
-    bank_account,
-    bank_account_number,
-    gender,
-    marital_status,
-    profile_pic,
-    joined_date,
-  } = req.body;
-  const profile = await Profile.create({
-    id,
-    user_id,
-    first_name,
-    last_name,
-    email,
-    address,
-    contact_number,
-    dob,
-    citizenship_number,
-    pan_number,
-    bank_account,
-    bank_account_number,
-    gender,
-    marital_status,
-    profile_pic,
-    joined_date,
-  });
-  if (profile) {
-    res.status(201).json({
-      id: profile.id,
-      address: profile.address,
-      contact_number: profile.contact_number,
-      dob: profile.dob,
-      citizenship_number: profile.contact_number,
-      pan_number: profile.pan_number,
-      bank_account: profile.bank_account,
-      bank_account_number: profile.bank_account_number,
-      gender: profile.gender,
-      marital_status: profile.marital_status,
-      profile_pic: profile.profile_pic,
-      joined_date: profile.joined_date,
+router.post("/user", isAdmin, async (req, res, next) => {
+  try {
+    let checkUser = await Profile.findOne({
+      where: {
+        user_id: req.user.id,
+      },
     });
-  } else {
-    res.status(400);
-    throw new Error("Profile not found");
-  }
-});
-router.get("/profile/:id", isAuth, async (req, res, next) => {
-  const profile = await Profile.findByPk(req.params.id);
-
-  if (profile) {
-    res.json(profile);
-  } else {
-    res.status(404).json({ message: "Profile not found" });
+    if (checkUser)
+      return res.status(404).json({ message: "Profile already exists" });
+    const {
+      id,
+      user_id,
+      first_name,
+      last_name,
+      email,
+      address,
+      contact_number,
+      dob,
+      citizenship_number,
+      pan_number,
+      bank_account,
+      bank_account_number,
+      gender,
+      marital_status,
+      profile_pic,
+      joined_date,
+    } = req.body;
+    const profile = await Profile.create({
+      id,
+      user_id: req.user.id,
+      first_name,
+      last_name,
+      email,
+      address,
+      contact_number,
+      dob,
+      citizenship_number,
+      pan_number,
+      bank_account,
+      bank_account_number,
+      gender,
+      marital_status,
+      profile_pic,
+      joined_date,
+    });
+    res.status(201).json({
+      profile,
+      message: "Details uploaded successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
